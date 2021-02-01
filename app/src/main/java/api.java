908c9831634bfd1a3486cd309776d931db8eb896
Phil <35858888+RxParalyze@users.noreply.github.com>
2021-01-31 19:18:00 -0500

    Built out devId and authKey importation from JSON file

--- app/src/main/java/api.java
+++ app/src/main/java/api.java
@@ -1,5 +1,6 @@
 import java.io.FileReader;
 
+import org.apache.http.cookie.SM;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -10,11 +11,22 @@ import kong.unirest.Unirest;
 public class API {
     public int devId;
     public String authKey;
+    public String sessionId;
+    public static final String SMITE_URL = "http://api.smitegame.com/smiteapi.svc";
 
     @SuppressWarnings("unchecked")
     public API () {
         JSONParser parser = new JSONParser();
 
+        /*
+        Reads in the developer information to connect to the SMITE API
+        To mimick this, create a file called "smite_dev_key.json" under the src/main/resources folder
+        in the file, insert the following:
+        {
+            "devId": xxxx,
+            "authKey": "xxxxxxxx..."
+        }
+        */
         try {
             Object obj = parser.parse(new FileReader("../resources/smite_dev_key.json"));
 
@@ -26,11 +38,18 @@ public class API {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //Creates a Session with your Dev Info
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public JsonNode apiPost()
     {
-        HttpResponse <JsonNode> httpResponse = Unirest.post("")
+        HttpResponse <JsonNode> httpResponse = Unirest.post(SMITE_URL)
             .header("name", "value")
             .queryString("name", "value")
             .field("name", "value")
@@ -42,7 +61,7 @@ public class API {
 
     public JsonNode apiGet()
     {
-        HttpResponse <JsonNode> httpResponse = Unirest.get("")
+        HttpResponse <JsonNode> httpResponse = Unirest.get(SMITE_URL)
             .header("name", "value")
             .queryString("name", "value")
             .asJson();
